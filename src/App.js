6ca852ec2e4fc13edc3a94b166a9e032091cdd9e
Ernesto Celis <ernesto@celisdelafuente.net>
2022-06-20 23:10:42 -0500

    Authorize user with token

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { useSearchParams } from 'react-router-dom';
 import Header from './components/Header';
 import SignIn from './components/Signin';
 import Calendar from './components/Calendar';
@@ -7,6 +8,21 @@ import axios from 'axios';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {
+    const token = searchParams.get('token');
+    if (token) {
+      axios.get('/login/callback?token=' + token, {}, {
+        headers: { 'Content-Type': 'application/json'}
+      })
+        .then(res =>{
+          setUser(res.data);
+        })
+        .catch(error => {
+          console.log('hubo pedo', error)
+        });
+    }
+  }, []);
 
   return (
     <div className="container">
